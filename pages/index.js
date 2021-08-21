7c0f6ba71a7ee0665d89cfc57a06b861b1a4dec4
Nihal Saxena <saxenanihal95@gmail.com>
2021-08-21 19:55:24 +0530

    Basic Structure setup

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,28 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function IndexPage() {
   const columns = [
-    { id: uuidv4(), title: "In Progress" },
-    { id: uuidv4(), title: "TODO" }
+    {
+      id: uuidv4(),
+      title: "In Progress",
+      items: [
+        {
+          title: "new item",
+          id: uuidv4()
+        }
+      ]
+    },
+    {
+      id: uuidv4(),
+      title: "TODO",
+      items: [
+        {
+          title: "new item 2",
+          id: uuidv4()
+        }
+      ]
+    }
   ];
-  const items = [];
+
   return (
     <div style={{ display: "flex", height: "100vh", overflowX: "scroll" }}>
       {columns.map((column) => (
@@ -15,10 +33,19 @@ export default function IndexPage() {
             minWidth: 250,
             backgroundColor: "gray",
             height: "100%",
-            margin: "0 10px"
+            margin: "0 10px",
+            padding: "0 10px"
           }}
         >
           {column.title}
+          {column?.items?.map((item) => (
+            <div
+              key={item.id}
+              style={{ backgroundColor: "blue", width: "100%", minHeight: 100 }}
+            >
+              {item.title}
+            </div>
+          ))}
         </div>
       ))}
     </div>
